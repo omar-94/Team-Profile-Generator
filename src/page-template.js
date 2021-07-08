2940b9e699f43b37e414b3b3ac0570f06185fd2e
onava <enava23@yahoo.com>
2021-07-08 00:42:39 -0400

    generate HTML code

--- src/page-template.js
+++ src/page-template.js
@@ -21,3 +21,34 @@ const generateEngineer = function(engineer) {
     </div>
     `
 }
+
+// HTML Starter Code
+const generateTeamPage = function (employees) {   
+    return`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Coding Team</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="jumbotron jumbotron-fluid bg-info">
+            <div class="container">
+                <h1 class="display-3"><strong>Coding Team</strong></h1>
+            </div>
+        </div>
+        <main>
+            <div class="container">
+                <div class="row justify-content-center" id="team-cards">
+                    ${employees}
+                </div>
+            </div>
+        </main>
+        
+    </body>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    </html>
+  `
+};
